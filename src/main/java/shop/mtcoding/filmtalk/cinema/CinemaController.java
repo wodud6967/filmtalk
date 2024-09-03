d17c82690b090d191f7a5c9c55f87ac91c607928
Hojeong <queen00220@naver.com>
2024-09-03 16:06:25 +0900

    .cineTest 코드추가

--- src/main/java/shop/mtcoding/filmtalk/cinema/CinemaController.java
+++ src/main/java/shop/mtcoding/filmtalk/cinema/CinemaController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.filmtalk.cinema;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CinemaController {
@@ -10,5 +11,11 @@ public class CinemaController {
     public String view(){
 
         return "cinema/veiw";
+    }
+
+    @ResponseBody
+    @GetMapping("/cineTest")
+    public String test(){
+        return "테스트 성공";
     }
 }
