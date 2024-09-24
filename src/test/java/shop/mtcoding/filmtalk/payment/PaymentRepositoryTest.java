21229b3e34c3d66a1913554bdd1b4f95476649f8
shnmj <shnmje@gmail.com>
2024-09-24 14:29:17 +0900

    쿼리 2 results were returned 에러 해결중

--- src/test/java/shop/mtcoding/filmtalk/payment/PaymentRepositoryTest.java
+++ src/test/java/shop/mtcoding/filmtalk/payment/PaymentRepositoryTest.java
@@ -15,8 +15,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Transactional
@@ -24,7 +22,6 @@ public class PaymentRepositoryTest {
 
     @Autowired
     private PaymentRepository paymentRepository;
-
     @Autowired
     private PaymentService paymentService;
     @Autowired
@@ -73,16 +70,4 @@ public class PaymentRepositoryTest {
         assertEquals("card", savedPayment.getType());
     }
 
-    @Test
-    public void testUpdatePayment() {
-        // Given : 결제 정보 DTO 준비
-        PaymentRequest.SaveDTO saveDTO = new PaymentRequest.SaveDTO();
-        saveDTO.setReservationId("1");
-        saveDTO.setImpUid("imp_28446715");
-
-        // When : 결제 정보 저장 로직 호출
-        paymentService.save(saveDTO);
-
-        // Then : 추가적으로 저장된 데이터를 조회해도 됨
-    }
 }
