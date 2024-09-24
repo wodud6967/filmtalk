f398f33e4707c24ea3f71cd8044edc7f39022ddc
shnmj <shnmje@gmail.com>
2024-09-24 16:33:19 +0900

    쿼리 2 result Error 해결 및 아임포트 결제요청 진행중

--- src/main/java/shop/mtcoding/filmtalk/payment/PaymentController.java
+++ src/main/java/shop/mtcoding/filmtalk/payment/PaymentController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
